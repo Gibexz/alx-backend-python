0bfa0216a555a73ecdbbcee4286efd5e119a92f8
Gibexz <andrewetedu@gmail.com>
2023-10-10 19:57:28 +0100

    Task: 1. Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,3 +2,15 @@
 """
 module: 1-async_comprehension.py
 """
+from typing import List
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[]:
+    """
+    collect 10 random numbers using an async comprehensing
+    over async_generator, then return the 10 random numbers.
+    """
+    random_num_list = [num async for num in async_generator()]
+    return random_num_list

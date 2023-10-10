d2b50523d9262917296a55e1db884481c87d890a
Gibexz <andrewetedu@gmail.com>
2023-10-10 19:23:58 +0100

    add new Project dir, readme file and and task: 0. Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+module: 0-async_generator.py
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    loop 10 times, each time asynchronously wait 1 second, then
+    yield a random number between 0 and 10. Use the random module
+    """
+    for i in range(10):
+        x = random.uniform(0, 10)
+        await asyncio.sleep(1)
+        yield x
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,2 @@
+0x02-python_async_comprehension
+ALX SE Short Specialization Project
